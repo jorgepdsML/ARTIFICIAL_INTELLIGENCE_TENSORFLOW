b443b7b21e6c2be0d087a11d557fa8c69eb34e59
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2019-12-18 19:33:27 -0500

    Update tensorflow_cond_less.py

--- TENSOR_COND/tensorflow_cond_less.py
+++ TENSOR_COND/tensorflow_cond_less.py
@@ -1,3 +1,4 @@
+#written by jorge orlando miranda ñahui
 import tensorflow as tf
 #use of graph and session
 tf.compat.v1.disable_eager_execution()
@@ -8,7 +9,7 @@ print(t1,t2)
 #use tf.cond(pred,f1(),f2())
 #returns a f1 if pred is true else f2
 c1=tf.cond(tf.less(t1,t2),lambda :tf.constant(0,tf.float32),lambda :tf.constant(1,tf.float32))
-#create a Session
+#create a Session using the context manager 
 with tf.compat.v1.Session(graph=tf.compat.v1.get_default_graph()) as se1:
     #run and print the result in the session
     print(se1.run(c1))
