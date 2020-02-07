99279c592168f74ee35edbb6352384a100049356
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2020-02-06 19:05:18 -0500

    Add files via upload

---
+++ CONVOLUTION2D_KERAS_BASIC/keras_convolucion.py
@@ -0,0 +1,36 @@
+"""
+PYTHON, NUMPY , TENSORFLOW AND KERAS
+written by jorge orlando miranda ñahui
+github:https://github.com/jorgepdsML/ARTIFICIAL_INTELLIGENCE_TENSORFLOW/upload/master
+youtube: jorge miranda redes neuronales
+Fanpage : BRAINTELS LABS FACEBOOK
+
+"""
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D
+#------------------construir el modelo
+modelo=Sequential()
+#--------------------Crear capa Conv2D
+layer1=Conv2D(filters=1,kernel_size=3,strides=1,padding="valid",input_shape=(4,4,1))
+#---------------agregar la capa Conv2D al modelo Sequential
+modelo.add(layer1)
+#dimensión del kernel (Nfilas,Ncolumnas,Ncanales,Nfilters)
+# (3 , 3 , 1 , 1)
+#------------------Inicializar el Kernel ConV2D
+W=np.array([[1,-1,1],
+            [2,5,10],
+            [0,1,2]],dtype=float).reshape((3,3,1,1))
+
+Ws=[W,np.array([0.0])]
+#--------------- Establecer Kernel Conv2D igual a W
+layer1.set_weights(Ws)
+#-------------------MATRIZ DE ENTRADA
+I=np.array([[10,15,0,10],
+            [20,0,0,10],
+            [0,1,10,20],
+            [0,10,20,10]],dtype=float).reshape((1,4,4,1))
+#--------------Realizar la convolución 2D
+Ir=modelo.predict(I)
+print("RESULTADO DE LA CONVOLUCIÓN ES: \n {} ".format(Ir))
+print(np.shape(Ir))
